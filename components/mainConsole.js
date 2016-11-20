1cbe968d3933a9ce4bc66fa80bf39cb19aca6a87
kkindorf <kevin.kindorf@gmail.com>
2016-11-20 11:32:55 -0500

    need to figure out how to change background color on compMove

--- components/mainConsole.js
+++ components/mainConsole.js
@@ -6,8 +6,9 @@ var MainConsole = React.createClass({
         <input type='text' id='move-count' value={this.props.numMoves} readOnly/>
         <div className="console-buttons">
           <h2 id="normal" onClick={this.props.onClick}>Normal Mode</h2>
-          <h2 id="restart" onClick={this.props.onClick}>Restart</h2>
+          <h2 id="restart" onClick={this.props.onRestart}>Restart</h2>
           <h2 id="strict" onClick={this.props.onClick}>Strict Mode</h2>
+          <h2 id="start" onClick={this.props.onStart}>Start</h2>
         </div>
       </div>
     )
--- components/simonContainer.js
+++ components/simonContainer.js
@@ -1,24 +1,74 @@
 var React = require('react');
 var MainConsole = require('./mainConsole.js');
 var BgBtnContainer = require('./bgContainer.js');
+var initialState = {
+                    normal: false,
+                    restart: false,
+                    strict: false,
+                    start: false,
+                    moveCount: '',
+                  };
 var SimonContainer = React.createClass({
-  onBtnType: function(event){
-    var id = event.target.id;
-    if(id==='1'){
+  getInitialState: function(){
+    return initialState;
+  },
+  getRandomId: function(){
+    return Math.floor(Math.random() * (4 - 1)) + 1;
+  },
+  playSound: function(num){
+    if(num === 1){
       this.refs.noiseOne.play();
     }
-    else if(id === '2'){
+    else if(num === 2){
       this.refs.noiseTwo.play();
     }
-    else if(id === '3'){
+    else if(num === 3){
       this.refs.noiseThree.play();
     }
-    else if(id === '4'){
+    else if(num === 4){
       this.refs.noiseFour.play();
     }
+
+  },
+  compMove: function(){
+    //figure out how to change background color next
+    var arr = [];
+    var num = this.getRandomId();
+    this.playSound(num);
+    arr.push(num);
+    //return arr for checking if human choice is correct
+  },
+  onBtnType: function(event){
+    var id = event.target.id;
+    id = parseInt(id);
+    if(this.state.start === false){
+      return;
+    }
+    this.playSound(id);
+  },
+  onStart: function(){
+    //start game
+    this.setState({start: true})
+  },
+  onRestart: function(){
+    //reset state
+    this.setState(this.getInitialState());
   },
   onWorkType: function(event){
-    console.log(event.target.id);
+    var id = event.target.id;
+    if(!this.state.start){
+      return;
+    }
+    if(id ==='normal'){
+      this.setState({normal: true, start: false});
+      this.compMove();
+      //run game function using normal flag
+    }
+    else if(id === 'strict'){
+      this.setState({strict: true, start: false});
+        this.compMove();
+      //run game function using strict flag
+    }
   },
   render: function(){
     return (
@@ -30,7 +80,9 @@ var SimonContainer = React.createClass({
               <audio ref="noiseFour" src="https://s3.amazonaws.com/freecodecamp/simonSound4.mp3" type="audio/mp3"></audio>
               <div className="game-container">
                 <BgBtnContainer onClick = {this.onBtnType}/>
-                <MainConsole onClick = {this.onWorkType}/>
+                <MainConsole onClick = {this.onWorkType}
+                             onStart = {this.onStart}
+                             onRestart = {this.onRestart}/>
               </div>
             </div>
           </div>
